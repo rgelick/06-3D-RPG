3a9c6890ef794aa60509bd8f5584a128a40caf00
rgelick <rgelick@iu.edu>
2022-04-30 23:36:41 -0400

    Complete Project

---
+++ UI/Game_Over.gd
@@ -0,0 +1,15 @@
+extends Control
+
+
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	$Label.text = "You died! Your score was " + str(Global.score)
+
+
+func _on_Play_pressed():
+	Global.reset()
+	get_tree().change_scene("res://Game.tscn")
+
+
+func _on_Quit_pressed():
+	get_tree().quit()
